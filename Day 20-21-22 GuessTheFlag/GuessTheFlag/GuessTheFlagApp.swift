e74e21fd8620aeaee41e40c3537b74fc6c625396
Harun Ünal <36730353+harununal@users.noreply.github.com>
2021-12-05 15:06:35 +0300

    Add files via upload

---
+++ Day 20-21-22 GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mac on 3.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
