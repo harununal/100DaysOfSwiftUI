6b3a7400f3e1989711d6ae3100d9187b00803503
Harun Ünal <36730353+harununal@users.noreply.github.com>
2022-01-02 18:46:48 +0300

    Add files via upload

---
+++ Day53-54-55-56-Bookworm/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Mac on 27.12.2021.
+//
+import CoreData
+import SwiftUI
+
+struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.title),
+        SortDescriptor(\.author)
+    ]) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        DetailView(book: book)
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                                .foregroundColor(book.rating == 1 ? Color.red.opacity(0.5) : Color.primary)
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknown Title")
+                                    .font(.headline)
+                                    .foregroundColor(book.rating == 1 ? Color.red : Color.primary)
+                                Text(book.author ?? "Unknown Author")
+                                    .foregroundColor(book.rating == 1 ? Color.red.opacity(0.5) : Color.secondary)
+                            }
+                        }
+                    }
+                }
+                .onDelete(perform: deleteBooks)
+            }
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showingAddScreen.toggle()
+                        } label: {
+                            Label("Add Book", systemImage: "plus")
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EditButton()
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
+        }
+    }
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our fetch request
+            let book = books[offset]
+
+            // delete it from the context
+            moc.delete(book)
+        }
+
+        // save the context
+        try? moc.save()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
